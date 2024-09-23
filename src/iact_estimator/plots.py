e561d9d61e4278450e51e65124db82b2ed4617b4
Michele Peresano <peresano@mpp.mpg.de>
2024-09-23 11:39:31 +0200

    Add plot function for SkyView images using astroplan

--- src/iact_estimator/plots.py
+++ src/iact_estimator/plots.py
@@ -6,7 +6,7 @@ from pathlib import Path
 import astropy.units as u
 from astropy.visualization import quantity_support
 from astroplan import FixedTarget
-from astroplan.plots import plot_sky_24hr, plot_altitude
+from astroplan.plots import plot_sky_24hr, plot_altitude, plot_finder_image
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -374,4 +374,85 @@ def plot_rates(performance_data, title=None, ax=None):
         ax.legend()
         ax.set_xscale("log")
         ax.set_yscale("log")
+    return ax
+
+
+@u.quantity_input(fov_radius=u.arcmin)
+def plot_from_skyview_survey(
+    target_source,
+    survey_name="DSS",
+    fov_radius=10 * u.arcmin,
+    log=False,
+    ax=None,
+    grid=False,
+    reticle=False,
+    style_kwargs=None,
+    reticle_style_kwargs=None,
+):
+    """Plot a survey image from the SkyView service centered on ``target``.
+
+    Parameters
+    ----------
+    target : `~astroplan.FixedTarget`, `~astropy.coordinates.SkyCoord`
+        Coordinates of celestial object
+
+    survey : string
+        Name of survey to retrieve image from. For dictionary of
+        available surveys, use
+        ``from astroquery.skyview import SkyView; SkyView.list_surveys()``.
+        Defaults to ``'DSS'``, the Digital Sky Survey.
+
+    fov_radius : `~astropy.units.Quantity`
+        Radius of field of view of retrieved image. Defaults to 10 arcmin.
+
+    log : bool, optional
+        Take the natural logarithm of the FITS image if `True`.
+        False by default.
+
+    ax : `~matplotlib.axes.Axes` or None, optional.
+        The `~matplotlib.axes.Axes` object to be drawn on.
+        If None, uses the current `~matplotlib.axes.Axes`.
+
+    grid : bool, optional.
+        Grid is drawn if `True`. `False` by default.
+
+    reticle : bool, optional
+        Draw reticle on the center of the FOV if `True`. Default is `False`.
+
+    style_kwargs : dict or `None`, optional.
+        A dictionary of keywords passed into `~matplotlib.pyplot.imshow`
+        to set plotting styles.
+
+    reticle_style_kwargs : dict or `None`, optional
+        A dictionary of keywords passed into `~matplotlib.pyplot.axvline` and
+        `~matplotlib.pyplot.axhline` to set reticle style.
+
+    Returns
+    -------
+    ax : `~matplotlib.axes.Axes`
+        Matplotlib axes with survey image centered on ``target``
+
+    hdu : `~astropy.io.fits.PrimaryHDU`
+        FITS HDU of the retrieved image
+
+    Notes
+    -----
+    This is wrapper function around `astroplan.plots.plot_finder_image()`.
+
+    """
+
+    ax = plt.gca() if ax is None else ax
+
+    ax = plot_finder_image(
+        target_source,
+        survey=survey_name,
+        fov_radius=fov_radius,
+        log=log,
+        ax=ax,
+        grid=grid,
+        reticle=reticle,
+        style_kwargs=style_kwargs,
+        reticle_style_kwargs=reticle_style_kwargs,
+    )
+
     return ax
