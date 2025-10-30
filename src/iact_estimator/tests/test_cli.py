967dad854e8225a0deb533541082abf31634ae22
Michele Peresano <peresano@mpp.mpg.de>
2025-10-30 11:43:08 +0100

    Add test_cli_run_galactic_coordinates

--- src/iact_estimator/tests/test_cli.py
+++ src/iact_estimator/tests/test_cli.py
@@ -1,6 +1,7 @@
 """Tests for the Command-line interface."""
 
 import matplotlib
+import yaml
 
 matplotlib.set_loglevel("critical")
 
@@ -23,6 +24,38 @@ def test_cli_config(script_runner, tmp_path):
 
 def test_cli_run(script_runner, tmp_path):
     script_runner.run(["iact-estimator", "config", "--to", tmp_path])
+    result = script_runner.run(
+        [
+            "iact-estimator",
+            "run",
+            "--config",
+            tmp_path / "config.yml",
+            "--source-name",
+            "Crab Nebula",
+            "--output-path",
+            tmp_path,
+        ],
+    )
+    assert result.returncode == 0
+
+
+def test_cli_run_galactic_coordinates(script_runner, tmp_path):
+    script_runner.run(["iact-estimator", "config", "--to", tmp_path])
+
+    # open the config file and modify the source coordinates to be in galactic system
+    config_path = tmp_path / "config.yml"
+    with open(config_path, "r") as f:
+        config = yaml.safe_load(f)
+
+    config["target_source"]["coordinates"] = {
+        "force": True,
+        "l": 184.56,
+        "b": -5.78,
+        "frame": "galactic",
+    }
+    with open(config_path, "w") as f:
+        yaml.safe_dump(config, f)
+
     result = script_runner.run(
         [
             "iact-estimator",
